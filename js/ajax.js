ff054db8e204629855c7089af29d724dc53a281e
Irck113 <rick.mc.fly2@gmail.com>
2018-05-22 14:46:57 -0500

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-    $("button").click(function(){
-        $.post("consulta.php",
-        {
-          nombre: "dasfas",
-          apPaterno: "afasd"
-        },
-        function(data,status){
-            alert("Data: " + data + "\nStatus: " + status);
-        });
-    });
-});
 
+	$("button").click(function(){
+
+		$.get("consultadb.php", function(data, status){
+
+			$("#usuarios").html(data);
+
+		});
+
+	});
+
+});
